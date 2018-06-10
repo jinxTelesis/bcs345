b92624106b4e5eed50abda357ba7b3f4ce78967f
jinxtelesis <dremon7@hotmail.com>
2018-06-09 21:36:36 -0400

    Initial commit for class gui project

---
+++ GuiProject/src/oop/Employee.java
@@ -0,0 +1,90 @@
+package oop;
+
+public class Employee {
+	private String firstName;
+	private String lastName;
+	private String deptName;
+	private static int ID;
+	private int eID;
+
+	Employee(){
+		this.firstName = "";
+		this.lastName = "";
+		this.deptName = "";
+		ID++;
+		this.eID = ID;
+	}
+	
+	Employee(String firstName, String lastName, String deptName)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.deptName = deptName;
+		this.eID = ID;
+	}
+	
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+	
+	public void setDeptName(String deptName)
+	{
+		this.deptName = deptName;
+	}
+	
+	public String getFirstName()
+	{
+		return this.firstName;
+	}
+	
+	public String getLastName()
+	{
+		return this.lastName;
+	}
+	
+	public String deptName()
+	{
+		return this.deptName;
+	}
+	
+	public String toString()
+	{
+		String s = null;
+		s+= "The employee's first name is: " + this.firstName +
+		"\n The employee's last name is: " + this.lastName +
+		"\n The employee works for the: " + this.deptName + " department " +
+		"\n The employee's id is : " + this.eID;
+		
+	return s;
+	}
+
+	public boolean equals(Object o)
+	{
+		if( o instanceof Employee)
+		{
+			Employee otherE = (Employee)o;
+			if(this.firstName == otherE.firstName) {
+				if(this.lastName == otherE.lastName) {
+					if(this.deptName == otherE.deptName)
+					{
+						if(this.eID == otherE.eID)
+							return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ GuiProject/src/oop/driver.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class driver {
+
+	public static void main(String[] args) {
+		Employee emp = new Employee("Dave ", "Likes ", "Carrots");
+		System.out.println(emp);
+
+	}
+
+}
