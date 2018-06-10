f3504ced0655b3f224c90f7b80ff2eed3273ac7a
jinxtelesis <dremon7@hotmail.com>
2018-06-10 14:06:33 -0400

    updates to grad and undergrade

---
+++ GuiProject/src/oop/Undergraduates.java
@@ -0,0 +1,14 @@
+package oop;
+
+public class Undergraduates extends Student {
+	
+	Undergraduates(){
+		super();// need courses only to be U // and status 
+	}
+	
+	Undergraduates(String firstName, String lastName, String[] coursesTaken)
+	{
+		
+	}
+
+}
