9b80faf86492e1c510e809a4c83439f9255adea1
jinxtelesis <dremon7@hotmail.com>
2018-06-11 16:15:59 -0400

    added display area and boarders

--- GuiProject/src/oop/GuiJPanel.java
+++ GuiProject/src/oop/GuiJPanel.java
@@ -1,19 +1,30 @@
 package oop;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+
 import java.util.Scanner;
 
 public class GuiJPanel extends JPanel implements ActionListener{
 	
-	Object[] objectarr = new Object[1000];
-	private static int objArrCount =0;
+	//Object[] objectarr = new Object[1000];
+	
+	// got this from java documentation 
+	Border blackline, raisedetched, loweredetched,
+    raisedbevel, loweredbevel, empty;
+
+	// got this from java documentation
+	
 	
 	public static final String Person = "Person";
 	public static final String Employee = "Employee";
@@ -30,10 +41,10 @@ public class GuiJPanel extends JPanel implements ActionListener{
 	private JButton jbCreate, jbRemove, jbUpdate, jbDisplay;
 	private JButton jbEnter;
 	private static final int NUM_COLUMNS = 10;
-	private String typeDisplay = "\n\n\n\n\n     Type currently \n        selected is";
-	private String selDisplay = " Person Selected ";
+	
 	private JTextArea jTAtypeDisplay;
 	private JTextArea jTASelectionDisplay;
+	private JTextArea jTADisData;
 	public Scanner scan = new Scanner(System.in);
 	public String UserInput = "";
 	
@@ -44,24 +55,54 @@ public class GuiJPanel extends JPanel implements ActionListener{
 	private Undergraduate u1[] = new Undergraduate[20];
 	private Graduate g1[] = new Graduate[20];
 	
+	private static int personArrCount =0; // refactored
+	private static int employeeArrCount =0;
+	private static int facultyArrCount = 0;
+	private static int studentArrCount = 0;
+	private static int underArrCount = 0;
+	private static int fsdsdf =0;
 	
 	//
 	private String objState = new String();
 	private String alterState = new String();
 	
+	private String typeDisplay = "\n\n\n\n\n     Type currently \n        selected is";
+	private String selDisplay = " Person Selected ";
+	private String jlOneDis = " ";
+	private String jlTwoDis = " ";
+	private String jlFourDis = " ";
+	private String jlFiveDis = " ";
+	private String jlTADisData = " ";
+	
 	public GuiJPanel() {
+		// from java documentation
+		blackline = BorderFactory.createLineBorder(Color.black);
+		raisedetched = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
+		loweredetched = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
+		raisedbevel = BorderFactory.createRaisedBevelBorder();
+		loweredbevel = BorderFactory.createLoweredBevelBorder();
+		empty = BorderFactory.createEmptyBorder();
+		// from java documentation 
 		
-		jlOne = new JLabel("");
-		jlTwo = new JLabel("");
+		
+		jlOne = new JLabel(jlOneDis);
+		jlTwo = new JLabel(jlTwoDis);
 		jlThree = new JLabel("Select a type of member to alter");
-		jlFour = new JLabel("");
-		jlFive = new JLabel("");
+		jlFour = new JLabel(jlFourDis);
+		jlFive = new JLabel(jlFiveDis);
 		jTAtypeDisplay = new JTextArea(typeDisplay, 10, 10);
 		jTAtypeDisplay.setLineWrap(true);
 		jTAtypeDisplay.setWrapStyleWord(true);
+		jTAtypeDisplay.setBorder(raisedbevel);
 		jTASelectionDisplay = new JTextArea(selDisplay, 5, 5);
 		jTASelectionDisplay.setLineWrap(true);
 		jTASelectionDisplay.setWrapStyleWord(true);
+		jTASelectionDisplay.setBorder(raisedbevel);
+		jTADisData = new JTextArea(jlTADisData, 5, 5);
+		jTADisData.setLineWrap(true);
+		jTADisData.setWrapStyleWord(true);
+		jTADisData.setBorder(loweredbevel);
+		jTADisData.setEditable(false);
 		
 		// add highlight for the data type
 		add(jlOne);
@@ -93,6 +134,7 @@ public class GuiJPanel extends JPanel implements ActionListener{
 		add(jbEnter);
 		
 		add(jTASelectionDisplay);
+		add(jTADisData);
 		
 		jbPer.addActionListener(this);
 		jbEmp.addActionListener(this);
@@ -171,43 +213,48 @@ public class GuiJPanel extends JPanel implements ActionListener{
 			switch(objState) // add text field or set up text field for display
 			{
 				case Person:
-					if(alterState.equalsIgnoreCase(" Creating ") && objArrCount < 10) // worked needs output message for past 10
+					if(alterState.equalsIgnoreCase(" Creating ") && personArrCount < 10) // worked needs output message for past 10
 					{
 						System.out.println("Person update worked " + objState);
 						System.out.println("Person update worked " + alterState);
 						
 						// need an incrementor for id?
-						p1[objArrCount] = new Person();
-						p1[objArrCount].setFirstName("Tom");
-						p1[objArrCount].setLastName("Brandy");
-						objArrCount++;
+						p1[personArrCount] = new Person();
+						p1[personArrCount].setFirstName("Tom");
+						p1[personArrCount].setLastName("Brandy");
+						personArrCount++;
 					}
 					
-					if(alterState.equalsIgnoreCase(" Removing ") && objArrCount > 0)
+					if(alterState.equalsIgnoreCase(" Removing ") && personArrCount > 0)
 					{
 						System.out.println("Person update worked " + objState);
 						System.out.println("Person update worked " + alterState);
 						
 						// need an incrementor for id?
-						p1[objArrCount].setFirstName("");
-						p1[objArrCount].setLastName("");
-						objArrCount--;
+						p1[personArrCount].setFirstName("");
+						p1[personArrCount].setLastName("");
+						personArrCount--;
 					}
 					
-					if(alterState.equalsIgnoreCase(" Updating ") && objArrCount > -1)
+					if(alterState.equalsIgnoreCase(" Updating ") && personArrCount > -1)
 					{
 						System.out.println("Person update worked " + objState);
 						System.out.println("Person update worked " + alterState);
 						
 						// need an incrementor for id?
-						p1[objArrCount].setFirstName("Tom");
-						p1[objArrCount].setLastName("Brady");
+						p1[personArrCount].setFirstName("Jim");
+						p1[personArrCount].setLastName("Candy");
 					}
 					
-					if(alterState.equals(" Displaying ") && objArrCount > -1)
+					if(alterState.equals(" Displaying ") && personArrCount > -1)
 					{
 						System.out.println("Person update worked " + objState);
 						System.out.println("Person update worked " + alterState);
+						jlTADisData = " worked !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ";
+						//private String jlOneDis = " ";
+						//private String jlTwoDis = " ";
+						//private String jlFourDis = " ";
+						//private String jlFiveDis = " ";
 					}
 					System.out.println("Person update worked " + objState);
 					System.out.println("Person update worked " + alterState);
