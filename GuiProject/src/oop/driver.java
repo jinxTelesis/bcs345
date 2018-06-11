d79baf40144b16b547dbbbb6ef47d03b21fc97cf
jinxtelesis <dremon7@hotmail.com>
2018-06-11 12:16:53 -0400

    objects tested correctly

--- GuiProject/src/oop/driver.java
+++ GuiProject/src/oop/driver.java
@@ -7,12 +7,16 @@ public class driver {
 
 	public static void main(String[] args) {
 		
+		String[] coursearr= new String[9];
 		Employee emp = new Employee("Dave ", "Likes ", "Carrots");
+		Faculty fac1 = new Faculty("George", "Bill", "Sales", coursearr);
 		emp.setFirstName("Jim");
+		fac1.setFirstName("George2");
 		
 		
 		System.out.println("Get test" + emp.getFirstName());
 		System.out.println("");
+		System.out.println("Get test fac1" + fac1.getFirstName());
 		
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			
