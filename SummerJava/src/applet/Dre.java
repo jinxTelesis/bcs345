f8d5965f59febde06e0c541717c27be7a3e62fcb
jinxtelesis <dremon7@hotmail.com>
2018-06-15 12:49:22 -0400

    test applet

---
+++ SummerJava/src/applet/Dre.java
@@ -0,0 +1,13 @@
+package applet;
+import java.awt.Graphics;
+
+import javax.swing.JApplet;
+
+public class Dre extends JApplet {
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.drawString("wow this actually worked?", 25, 25);
+	}
+
+}
